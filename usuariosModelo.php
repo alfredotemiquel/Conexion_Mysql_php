a1a7c20926419637a04276999a6b4c2df2c7e312
Alfredo Temiquel <atemiquelt@cenidet.edu.mx>
2014-04-02 14:07:33 -0600

    Create usuariosModelo.php

---
+++ usuariosModelo.php
@@ -0,0 +1,20 @@
+<?php  
+require_once "Modelo.php"; 
+
+class usuariosModelo extends Modelo 
+{     
+    public function __construct() 
+    { 
+        parent::__construct(); 
+    } 
+
+    public function get_users() 
+    { 
+        $result = $this->_db->query('SELECT * FROM usuarios'); 
+         
+        $users = $result->fetch_all(MYSQLI_ASSOC); 
+         
+        return $users; 
+    } 
+} 
+  ?> 
